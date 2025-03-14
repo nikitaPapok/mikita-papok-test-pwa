1608a8adff4ae972031d098194111619c14faf82
mikitapapok31600 <shadowjedimaster725@gmail.com>
2025-03-14 09:19:41 +0400

    add expand animation

--- src/utils/mock-data.js
+++ src/utils/mock-data.js
@@ -2,7 +2,8 @@ export const offers = [
   {
     id: 1,
     title: "Move the borders of reality!",
-    description: "Go on a space adventure - it's possible with us!",
+    description:
+      "Go on a space adventure - it's possible with us!\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     image: "/space-1.webp",
     gridArea: "content__container__element--first",
   },
